ac515c6965d25658a2e7d622c26f25bf1ede3947
Ken <kennethlieu@gmail.com>
2022-09-24 16:43:37 -0600

    Basic property object made

---
+++ Lab2/src/PropertyAssessment.java
@@ -0,0 +1,46 @@
+public class PropertyAssessment {
+    private int accountNum;
+    private int suite;
+    private int houseNum;
+    private String streetName;
+    private boolean garage;
+    private int neighID;
+    private String neighName;
+    private String ward;
+    private int assessment;
+    private String lat;
+    private String lon;
+    private String point;
+    private int assess1P;
+    private int assess2P;
+    private int assess3P;
+    private String assess1Name;
+    private String assess2Name;
+    private String assess3Name;
+
+    public PropertyAssessment(){        // Default object creation
+        this(0,0,0,"",false,0,"","",0,"","","",0,0,0,"","","");
+    }
+
+    public PropertyAssessment(int accountNum, int suite, int houseNum, String streetName, boolean garage, int neighID, String neighName, String ward, int assessment, String lat, String lon, String point, int assess1P, int assess2P, int assess3P, String assess1Name, String assess2Name, String assess3Name){
+        this.accountNum = accountNum;
+        this.suite = suite;
+        this.houseNum = houseNum;
+        this.streetName = streetName;
+        this.garage = garage;
+        this.neighID = neighID;
+        this.neighName = neighName;
+        this.ward = ward;
+        this.assessment = assessment;
+        this.lat = lat;
+        this.lon = lon;
+        this.point = point;
+        this.assess1P = assess1P;
+        this.assess2P = assess2P;
+        this.assess3P = assess3P;
+        this.assess1Name = assess1Name;
+        this.assess2Name = assess2Name;
+        this.assess3Name = assess3Name;
+
+    }
+}
