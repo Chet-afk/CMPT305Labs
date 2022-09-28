994dea4ac0feb2c04151485704fe318a60b8939a
unknown <kennethlieu@gmail.com>
2022-09-28 08:04:09 -0600

    Section to check for int only input

--- Lab/src/Lab2Main.java
+++ Lab/src/Lab2Main.java
@@ -16,16 +16,16 @@ public class Lab2Main {
 
             System.out.println("Enter CSV Name or Absolute Path: ");
 
-            Scanner inputFilePath = new Scanner(System.in);  // Sets up Scanner object that reads from Std Input
+            Scanner userInput = new Scanner(System.in);  // Sets up Scanner object that reads from Std Input
 
-            String CSVName = inputFilePath.nextLine(); // Prompt user input
+            String CSVName = userInput.nextLine(); // Prompt user input
             Path CSVPaths = Paths.get(CSVName);     // Get the path of the CSV and create a Path object from it
 
             System.out.println("The File Path is " + CSVPaths.toAbsolutePath());
 
             PropertyAssessment[] propertyValues = formatData(CSVPaths);
 
-            // Function calls section
+            // Descriptive Statistics sections for all properties
             System.out.println("Descriptive Statistics of all property Assessments");
             System.out.println("There are "+ numOfLines(propertyValues) + " recorded properties.");  // Returns how many Properties are assessed by counting the total amount of lines
 
@@ -35,7 +35,16 @@ public class Lab2Main {
             System.out.println("The range is $" + NumberFormat.getIntegerInstance().format(range(lowestAndHighest)));
             System.out.println("The average assessment value is: $" + NumberFormat.getIntegerInstance().format(mean(propertyValues)));
             System.out.println("The median value is: $" + NumberFormat.getIntegerInstance().format(median(propertyValues)));
+            System.out.println();
             //assessClass(propertyValues);
+
+            // Find a specific Account Number
+            System.out.println("Find a property assessment by account number: ");
+            String accNum = userInput.nextLine();
+
+            if accNum.matches("[0-9]+"){
+                
+            }
         }
         catch(Exception e){
             System.out.println("The filename is invalid.");
