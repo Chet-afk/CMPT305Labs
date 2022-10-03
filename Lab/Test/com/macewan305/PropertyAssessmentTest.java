a3b0eca342fd008ff17265fe106abdcb52a1bdfc
unknown <kennethlieu@gmail.com>
2022-10-03 17:57:05 -0600

    Added Testing for the PropertyAssessment Class

---
+++ Lab/Test/com/macewan305/PropertyAssessmentTest.java
@@ -0,0 +1,112 @@
+package com.macewan305;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PropertyAssessmentTest {
+
+    @Test
+    void accountNum() {
+    }
+
+    @Test
+    void getSuite() {
+    }
+
+    @Test
+    void getHouseNum() {
+    }
+
+    @Test
+    void getStreet() {
+    }
+
+    @Test
+    void garage() {
+    }
+
+    @Test
+    void neighbourhoodID() {
+    }
+
+    @Test
+    void neighbourhoodName() {
+    }
+
+    @Test
+    void getWard() {
+    }
+
+    @Test
+    void assessmentVal() {
+    }
+
+    @Test
+    void latitude() {
+    }
+
+    @Test
+    void longitude() {
+    }
+
+    @Test
+    void point() {
+    }
+
+    @Test
+    void assess1P() {
+    }
+
+    @Test
+    void assess2P() {
+    }
+
+    @Test
+    void assess3P() {
+    }
+
+    @Test
+    void assess1Name() {
+    }
+
+    @Test
+    void assess2Name() {
+    }
+
+    @Test
+    void assess3Name() {
+    }
+
+    @Test
+    void address() {
+    }
+
+    @Test
+    void location() {
+    }
+
+    @Test
+    void allClasses() {
+    }
+
+    @Test
+    void area() {
+    }
+
+    @Test
+    void compareAssessValue() {
+    }
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
+}
